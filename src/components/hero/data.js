aa9363fbcd4ecdf4342911f1ecfd545f331278f0
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-09-14 11:15:40 -0700

    first commit

---
+++ src/components/hero/data.js
@@ -0,0 +1,33 @@
+const imageSlide = [
+  {
+    url: "pic-1.jpg",
+    title: "",
+    body: "",
+  },
+  {
+    url: "pic-2.jpg",
+    title: "",
+    body: "",
+  },
+  {
+    url: "pic-3.jpg",
+    title: "",
+    body: "",
+  },
+  {
+    url: "pic-4.jpg",
+    title: "",
+    body: "",
+  },
+  {
+    url: "pic-5.jpg",
+    title: "",
+    body: "",
+  },
+  {
+    url: "pic-6.jpg",
+    title: "",
+    body: "",
+  },
+];
+export default imageSlide;
