6871b0860ae9d77d58f08b2beee46c2dc1f472df
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-11-30 09:41:46 -0800

    update

--- src/components/customBouquets/products.js
+++ src/components/customBouquets/products.js
@@ -1,34 +1,56 @@
 import React from "react";
 import "./products.css";
+import { useInView } from "react-intersection-observer";
 
-const ProductCard = ({ imageSrc, title }) => (
-  <div className="card">
-    <img src={imageSrc} alt={title} />
-    <h2>{title}</h2>
-  </div>
-);
+const ProductCard = ({ imageSrc, title, price }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+  });
+
+  return (
+    <div>
+      <div ref={ref} className={`card ${inView ? "zoomIn" : "zoomOut"}`}>
+        <img src={imageSrc} alt={title} />
+        <h2>{title}</h2>
+      </div>
+      <div
+        ref={ref}
+        className={`product-details ${inView ? "zoomIn" : "zoomOut"}`}
+      >
+        <h3>{price}</h3>
+        <h2>{title}</h2>
+        <button>Request</button>
+      </div>
+    </div>
+  );
+};
 
 const Products = () => {
   const productsData = [
     {
       imageSrc: "pic-1.jpg",
       title: "Product 1",
+      price: "$150",
     },
     {
       imageSrc: "pic-2.jpg",
       title: "Product 2",
+      price: "$150",
     },
     {
       imageSrc: "url_to_your_image_3",
       title: "Product 3",
+      price: "$150",
     },
     {
       imageSrc: "url_to_your_image_4",
       title: "Product 4",
+      price: "$150",
     },
     {
       imageSrc: "url_to_your_image_5",
       title: "Product 5",
+      price: "$150",
     },
   ];
 
@@ -37,7 +59,6 @@ const Products = () => {
       {productsData.map((product, index) => (
         <div className="emptyDiv" key={index}>
           <ProductCard {...product} />
-          <h2>{product.title}</h2>
         </div>
       ))}
     </div>
