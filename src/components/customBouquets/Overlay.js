5cbb3411e7080e922433e6d0c4a67bc3bdc6e9c8
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-12-04 09:13:50 -0800

    update

--- src/components/customBouquets/Overlay.js
+++ src/components/customBouquets/Overlay.js
@@ -1,13 +1,28 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import "./products.css";
 
 export function Overlay({ isOpen, onClose, children }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      // Use setTimeout to delay the application of the 'show' class
+      const timerId = setTimeout(() => {
+        setIsVisible(true);
+      }, 10);
+
+      return () => clearTimeout(timerId);
+    } else {
+      setIsVisible(false);
+    }
+  }, [isOpen]);
+
   return (
     <Fragment>
       {isOpen && (
         <div className="overlay">
           <div className="overlay__background" onClick={onClose} />
-          <div className={`overlay__container ${isOpen ? "show" : ""}`}>
+          <div className={`overlay__container ${isVisible ? "show" : ""}`}>
             <div className="overlay__controls">
               <button
                 className="overlay__close"
