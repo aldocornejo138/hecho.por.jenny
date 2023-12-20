f9872ce4599db83cfe1e2cb5a1f6dd941f700319
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-12-19 18:48:18 -0800

    update

--- src/components/customBouquets/productsData.js
+++ src/components/customBouquets/productsData.js
@@ -1,29 +1,9 @@
 import { p1, p2, p3, p4, p5, p6, p7, p8, p9, p10 } from "./Images/index.js";
-import {
-  p11,
-  p12,
-  p13,
-  p14,
-  p15,
-  p16,
-  p17,
-  p18,
-  p19,
-  p20,
-} from "./Images/index.js";
-import {
-  p21,
-  p22,
-  p23,
-  p24,
-  p25,
-  p26,
-  p27,
-  p28,
-  p29,
-  p30,
-  p31,
-} from "./Images/index.js";
+import { p11, p12, p13, p14, p15, p16, p17, p18, p19 } from "./Images/index.js";
+import { p20, p21, p22, p23, p24, p25, p26, p27, p28 } from "./Images/index.js";
+import { p29, p30, p31, p32, p33, p34, p35, p36, p37 } from "./Images/index.js";
+import { p38, p39, p40, p41, p42, p43, p44, p45, p46 } from "./Images/index.js";
+import { p47, p48, p49, p50, p51, p52, p53, p54, p55 } from "./Images/index.js";
 
 const productsData = [
   {
@@ -211,6 +191,145 @@ const productsData = [
   {
     id: "  ",
     imageSrc: p31,
+    title: "Product 5",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p32,
+    title: "Product 4",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p33,
+    title: "Product 5",
+    price: "149.99",
+  },
+  {
+    id: "price_1OMEvyI1zNR8sZYDu7Iv8yqk",
+    imageSrc: p34,
+    title: "Product 1",
+    price: "149.99",
+  },
+  {
+    id: "price_1OMEy0I1zNR8sZYD2Przz5fx",
+    imageSrc: p35,
+    title: "Product 2",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p36,
+    title: "Product 3",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p37,
+    title: "Product 4",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p38,
+    title: "Product 5",
+    price: "149.99",
+  },
+
+  {
+    id: "price_1OMEvyI1zNR8sZYDu7Iv8yqk",
+    imageSrc: p39,
+    title: "Product 1",
+    price: "149.99",
+  },
+  {
+    id: "price_1OMEy0I1zNR8sZYD2Przz5fx",
+    imageSrc: p40,
+    title: "Product 2",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p41,
+    title: "Product 3",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p42,
+    title: "Product 4",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p43,
+    title: "Product 5",
+    price: "149.99",
+  },
+  {
+    id: "price_1OMEy0I1zNR8sZYD2Przz5fx",
+    imageSrc: p44,
+    title: "Product 2",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p45,
+    title: "Product 3",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p46,
+    title: "Product 4",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p47,
+    title: "Product 5",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p48,
+    title: "Product 4",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p49,
+    title: "Product 5",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p50,
+    title: "Product 3",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p51,
+    title: "Product 4",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p52,
+    title: "Product 5",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p53,
+    title: "Product 4",
+    price: "149.99",
+  },
+  {
+    id: "  ",
+    imageSrc: p54,
     title: "Product 5",
     price: "149.99",
   },
