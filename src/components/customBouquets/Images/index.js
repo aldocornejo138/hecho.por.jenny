fbfdc56991a1a6245265da3491e3aa9fc71f0d10
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2024-01-23 11:14:10 -0800

    update

--- src/components/customBouquets/Images/index.js
+++ src/components/customBouquets/Images/index.js
@@ -1,12 +1,11 @@
 import p1 from "./p1.jpg";
 import p2 from "./p2.jpg";
-import p3 from "./p3.jpg";
 import p4 from "./p4.jpg";
 import p5 from "./p5.jpg";
 import p6 from "./p6.jpg";
 import p7 from "./p7.jpg";
 import p8 from "./p8.jpg";
-import p9 from "./p9.jpg";
+
 import p10 from "./p10.jpg";
 import p11 from "./p11.jpg";
 import p12 from "./p12.jpg";
@@ -25,44 +24,40 @@ import p24 from "./p24.jpg";
 import p25 from "./p25.jpg";
 import p26 from "./p26.jpg";
 import p27 from "./p27.jpg";
-import p28 from "./p28.jpg";
+
 import p29 from "./p29.jpg";
 import p30 from "./p30.jpg";
 import p31 from "./p31.jpg";
 import p32 from "./p32.jpg";
 import p33 from "./p33.jpg";
-import p34 from "./p34.jpg";
-import p35 from "./p35.jpg";
+
 import p36 from "./p36.jpg";
 import p37 from "./p37.jpg";
 import p38 from "./p38.jpg";
 import p39 from "./p39.jpg";
-import p40 from "./p40.jpg";
-import p41 from "./p41.jpg";
+
 import p42 from "./p42.jpg";
 import p43 from "./p43.jpg";
 import p44 from "./p44.jpg";
 import p45 from "./p45.jpg";
-import p46 from "./p46.jpg";
+
 import p47 from "./p47.jpg";
-import p48 from "./p48.jpg";
+
 import p49 from "./p49.jpg";
-import p50 from "./p50.jpg";
+
 import p51 from "./p51.jpg";
-import p52 from "./p52.jpg";
 import p53 from "./p53.jpg";
-import p54 from "./p54.jpg";
-import p55 from "./p55.jpg";
+
 import p56 from "./p56.jpg";
 import p57 from "./p57.jpg";
-import p58 from "./p58.jpg";
+
 import p59 from "./p59.jpg";
 import p60 from "./p60.jpg";
 import p61 from "./p16.jpg";
 import p62 from "./p62.jpg";
 import p63 from "./p63.jpg";
 import p64 from "./p64.jpg";
-import p65 from "./p65.jpg";
+
 import p66 from "./p66.jpg";
 import p67 from "./p67.jpg";
 import p68 from "./p68.jpg";
@@ -72,18 +67,29 @@ import p71 from "./p71.jpg";
 import p72 from "./p72.jpg";
 import p73 from "./p73.jpg";
 import p74 from "./p74.jpg";
-import p75 from "./p75.jpg";
+
 import p76 from "./p76.jpg";
+import p77 from "./p77.jpg";
+import p78 from "./p78.jpg";
+import p79 from "./p79.jpg";
+import p80 from "./p80.jpg";
+import p81 from "./p81.jpg";
+import p82 from "./p82.jpg";
+import p83 from "./p83.jpg";
+import p84 from "./p84.jpg";
+import p85 from "./p85.jpg";
+import p86 from "./p86.jpg";
+import p87 from "./p87.jpg";
 
 export { p1 };
 export { p2 };
-export { p3 };
+
 export { p4 };
 export { p5 };
 export { p6 };
 export { p7 };
 export { p8 };
-export { p9 };
+
 export { p10 };
 export { p11 };
 export { p12 };
@@ -101,45 +107,42 @@ export { p23 };
 export { p24 };
 export { p25 };
 export { p26 };
+
 export { p27 };
-export { p28 };
 export { p29 };
 export { p30 };
 export { p31 };
 export { p32 };
 export { p33 };
-export { p34 };
-export { p35 };
+
 export { p36 };
 export { p37 };
 export { p38 };
 export { p39 };
-export { p40 };
-export { p41 };
+
 export { p42 };
 export { p43 };
 export { p44 };
 export { p45 };
-export { p46 };
+
 export { p47 };
-export { p48 };
+
 export { p49 };
-export { p50 };
+
 export { p51 };
-export { p52 };
+
 export { p53 };
-export { p54 };
-export { p55 };
+
 export { p56 };
 export { p57 };
-export { p58 };
+
 export { p59 };
 export { p60 };
 export { p61 };
 export { p62 };
 export { p63 };
 export { p64 };
-export { p65 };
+
 export { p66 };
 export { p67 };
 export { p68 };
@@ -149,5 +152,17 @@ export { p71 };
 export { p72 };
 export { p73 };
 export { p74 };
-export { p75 };
+
 export { p76 };
+
+export { p77 };
+export { p78 };
+export { p79 };
+export { p80 };
+export { p81 };
+export { p82 };
+export { p83 };
+export { p84 };
+export { p85 };
+export { p86 };
+export { p87 };
--- src/components/customBouquets/productsData.js
+++ src/components/customBouquets/productsData.js
@@ -1,13 +1,13 @@
-import { p1, p2, p3, p4, p5, p6, p7, p8, p9, p10 } from "./Images/index.js";
+import { p1, p2, p4, p5, p6, p7, p8, p10 } from "./Images/index.js";
 import { p11, p12, p13, p14, p15, p16, p17, p18, p19 } from "./Images/index.js";
-import { p20, p21, p22, p23, p24, p25, p26, p27, p28 } from "./Images/index.js";
-import { p29, p30, p31, p32, p33, p34, p35, p36, p37 } from "./Images/index.js";
-import { p38, p39, p40, p41, p42, p43, p44, p45, p46 } from "./Images/index.js";
-import { p47, p48, p49, p50, p51, p52, p53, p54, p55 } from "./Images/index.js";
-import { p56, p57, p58, p59, p60, p61, p62, p63, p64 } from "./Images/index.js";
-import { p65, p66, p67, p68, p69, p70, p71, p72, p73 } from "./Images/index.js";
-import { p74, p75, p76 } from "./Images/index.js";
-
+import { p20, p21, p22, p23, p24, p25, p26, p27 } from "./Images/index.js";
+import { p29, p30, p31, p32, p33, p36, p37 } from "./Images/index.js";
+import { p38, p39, p42, p43, p44, p45 } from "./Images/index.js";
+import { p47, p49, p51, p53 } from "./Images/index.js";
+import { p56, p57, p59, p60, p62, p63, p64 } from "./Images/index.js";
+import { p66, p67, p68, p69, p70, p71, p72, p73 } from "./Images/index.js";
+import { p74, p76, p77, p78, p79, p80, p81, p82 } from "./Images/index.js";
+import { p83, p84, p85, p86, p87 } from "./Images/index.js";
 const productsData = [
   {
     id: "price_1OPpNlI1zNR8sZYDlf1fyOxc",
@@ -17,40 +17,162 @@ const productsData = [
   },
   {
     id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
-    imageSrc: p76,
-    title: "100 Roses",
-    price: "149.99",
+    imageSrc: p77,
+    title: "Floral Bear",
+    price: "290",
   },
   {
-    id: "price_1ORHAXI1zNR8sZYDC0bzg1lu",
-    imageSrc: p75,
-    title: "The Grinch",
-    price: "149.99",
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p78,
+    title: "Medium Mix",
+    price: "130",
+  },
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p79,
+    title: "50 Rose Love Wife",
+    price: "160",
+  },
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p80,
+    title: "25 Rose Birthday",
+    price: "110",
+  },
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p81,
+    title: "50 Rose B/W Dior ",
+    price: "155",
+  },
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p82,
+    title: "Hello Kitty(Flowers)",
+    price: "100",
+  },
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p83,
+    title: "25 Rose Birthday",
+    price: "85",
+  },
+
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p84,
+    title: "50 Rose Crown BDay",
+    price: "160",
+  },
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p85,
+    title: "50 Lily&Rose",
+    price: "175",
+  },
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p86,
+    title: "25Rose MinMickey",
+    price: "90",
+  },
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p76,
+    title: "100 Rose Bouquet",
+    price: "275",
   },
 
   {
     id: "price_1OPqxVI1zNR8sZYDMNtpMmh6",
     imageSrc: p74,
-    title: "Virgin Maria",
-    price: "149.99",
+    title: "25 Rose Glitter",
+    price: "95",
+  },
+  {
+    id: "price_1ORHBgI1zNR8sZYDlD9udAEw",
+    imageSrc: p10,
+    title: "Olla De Barro Mix",
+    price: "200",
+  },
+  {
+    id: "price_1OPprTI1zNR8sZYDDL4gVnTL",
+    imageSrc: p17,
+    title: "50Rose 21stBday",
+    price: "160",
+  },
+  {
+    id: "price_1OPprTI1zNR8sZYDDL4gVnTL",
+    imageSrc: p87,
+    title: "50Rose BBreaths ",
+    price: "170",
+  },
+  {
+    id: "price_1OPpfCI1zNR8sZYDnjj1skP4",
+    imageSrc: p7,
+    title: "25 Glitter&None Mix",
+    price: "95",
+  },
+  {
+    id: "price_1OPpqSI1zNR8sZYDSPfdZjIj",
+    imageSrc: p16,
+    title: "50 Red Roses",
+    price: "150",
+  },
+  {
+    id: "price_1OPpnMI1zNR8sZYD61iYpHko",
+    imageSrc: p12,
+    title: "Tiana Bouquet",
+    price: "105",
+  },
+  {
+    id: "price_1OPpsTI1zNR8sZYDgvHKOZeS",
+    imageSrc: p18,
+    title: "50 Rose Conmigo?",
+    price: "155",
+  },
+  {
+    id: "price_1OPq3ZI1zNR8sZYDWLVwqJub",
+    imageSrc: p30,
+    title: "75 Rose Pink Bow",
+    price: "180",
+  },
+  {
+    id: "price_1OPq2gI1zNR8sZYD8cEPOAdo",
+    imageSrc: p29,
+    title: "Winnie The Pooh",
+    price: "95",
+  },
+  {
+    id: "price_1OPqKbI1zNR8sZYD8UMcUMVV",
+    imageSrc: p51,
+    title: "50 Red and White",
+    price: "155",
+  },
+
+  {
+    id: "price_1OPqIDI1zNR8sZYDAxSZKFTW",
+    imageSrc: p47,
+    title: "25 Rose Te Amo",
+    price: "135",
+  },
+  {
+    id: "price_1OPqOwI1zNR8sZYDDUHqffEv",
+    imageSrc: p57,
+    title: "Sun & 25Rose",
+    price: "135",
   },
 
   {
     id: "price_1OMEy0I1zNR8sZYD2Przz5fx",
     imageSrc: p1,
-    title: "No Hay Otra",
-    price: "149.99",
+    title: "25Rose NoHayOtra",
+    price: "99",
   },
   {
     id: "price_1OPpPgI1zNR8sZYDZwOqYbbu",
     imageSrc: p2,
     title: "Anniversary White",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPpWkI1zNR8sZYDXfZG3U5q",
-    imageSrc: p3,
-    title: "NutCracker Pink",
     price: "149.99",
   },
   {
@@ -72,28 +194,11 @@ const productsData = [
     title: "Birthday Cream ",
     price: "149.99",
   },
-  {
-    id: "price_1OPpfCI1zNR8sZYDnjj1skP4",
-    imageSrc: p7,
-    title: "Birthday Dior Red",
-    price: "149.99",
-  },
+
   {
     id: "price_1OPpg9I1zNR8sZYDFpXDWwkC",
     imageSrc: p8,
     title: "Birthday Betty Boop",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPpkQI1zNR8sZYDzDy2Ep4j",
-    imageSrc: p9,
-    title: "Ginger Bread",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPplRI1zNR8sZYDQqoTwt2v",
-    imageSrc: p10,
-    title: "Pink Flower Pot",
     price: "149.99",
   },
   {
@@ -102,12 +207,7 @@ const productsData = [
     title: "Anniversary",
     price: "149.99",
   },
-  {
-    id: "price_1OPpnMI1zNR8sZYD61iYpHko",
-    imageSrc: p12,
-    title: "Princess & the Frog",
-    price: "149.99",
-  },
+
   {
     id: "price_1OPpo6I1zNR8sZYDCiqCJU13",
     imageSrc: p13,
@@ -126,24 +226,7 @@ const productsData = [
     title: "Teddy&Flowers",
     price: "149.99",
   },
-  {
-    id: "price_1OPpqSI1zNR8sZYDSPfdZjIj",
-    imageSrc: p16,
-    title: "Red Roses",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPprTI1zNR8sZYDDL4gVnTL",
-    imageSrc: p17,
-    title: "21st Crown Birthday",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPpsTI1zNR8sZYDgvHKOZeS",
-    imageSrc: p18,
-    title: "Casate Conmigo?",
-    price: "149.99",
-  },
+
   {
     id: "price_1OPptNI1zNR8sZYDJvzph3Wr",
     imageSrc: p19,
@@ -172,7 +255,7 @@ const productsData = [
   {
     id: "price_1OPpweI1zNR8sZYDVuXGn8wH",
     imageSrc: p23,
-    title: "Winnie Pooh",
+    title: "WinniePooh&Photos",
     price: "149.99",
   },
   {
@@ -199,24 +282,7 @@ const productsData = [
     title: "Roses Blue/White",
     price: "149.99",
   },
-  {
-    id: "price_1OPq1qI1zNR8sZYDAcbMYZae",
-    imageSrc: p28,
-    title: "Fall Bouquet",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPq2gI1zNR8sZYD8cEPOAdo",
-    imageSrc: p29,
-    title: "Winnie Pooh",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPq3ZI1zNR8sZYDWLVwqJub",
-    imageSrc: p30,
-    title: "PinkBow WhiteRose",
-    price: "149.99",
-  },
+
   {
     id: "price_1OPq4DI1zNR8sZYDmxTc0x9R",
     imageSrc: p31,
@@ -233,18 +299,6 @@ const productsData = [
     id: "price_1OPq5yI1zNR8sZYDo8tuzBFl",
     imageSrc: p33,
     title: "Money Hat",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPq6zI1zNR8sZYDQuyvyB0h",
-    imageSrc: p34,
-    title: "Scream in Pink",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPq7yI1zNR8sZYD9e13pTlX",
-    imageSrc: p35,
-    title: "Bday Fall Bouquet",
     price: "149.99",
   },
   {
@@ -273,18 +327,6 @@ const productsData = [
     price: "149.99",
   },
   {
-    id: "price_1OPqBxI1zNR8sZYDCnjLo1O7",
-    imageSrc: p40,
-    title: "Hello Kitty Basket",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPqCnI1zNR8sZYD565AV6kh",
-    imageSrc: p41,
-    title: "Evil Hello Kitty",
-    price: "149.99",
-  },
-  {
     id: "price_1OPqDSI1zNR8sZYDk8weCCLZ",
     imageSrc: p42,
     title: "Spider-Verse",
@@ -309,33 +351,9 @@ const productsData = [
     price: "149.99",
   },
   {
-    id: "price_1OPqHBI1zNR8sZYDeMTUX5ZG",
-    imageSrc: p46,
-    title: "Chucky Bouquet",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPqIDI1zNR8sZYDAxSZKFTW",
-    imageSrc: p47,
-    title: "Te Amo",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPqIrI1zNR8sZYDeKxx6GWa",
-    imageSrc: p48,
-    title: "Be My Devil",
-    price: "149.99",
-  },
-  {
     id: "price_1OPqJaI1zNR8sZYDrZ7E995z",
     imageSrc: p49,
     title: "WhitePink Butterfly",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPqKBI1zNR8sZYDifoqmA6N",
-    imageSrc: p50,
-    title: "Love You To Death",
     price: "149.99",
   },
   {
@@ -345,27 +363,9 @@ const productsData = [
     price: "149.99",
   },
   {
-    id: "price_1OPqLSI1zNR8sZYD6oGEKik1",
-    imageSrc: p52,
-    title: "Forever My Boo",
-    price: "149.99",
-  },
-  {
     id: "price_1OPqLzI1zNR8sZYDT5BOwslv",
     imageSrc: p53,
     title: "Lottery Hat",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPqMjI1zNR8sZYDkNb86yBk",
-    imageSrc: p54,
-    title: "Blue Scream",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPqNLI1zNR8sZYDC1lPIKxg",
-    imageSrc: p55,
-    title: "Michael Myers",
     price: "149.99",
   },
   {
@@ -381,12 +381,6 @@ const productsData = [
     price: "149.99",
   },
   {
-    id: "price_1OPqPRI1zNR8sZYD8UWp0kGf",
-    imageSrc: p58,
-    title: "Pink Bat Bouquet",
-    price: "149.99",
-  },
-  {
     id: "price_1OPqPqI1zNR8sZYDs3UWomNX",
     imageSrc: p59,
     title: "Premium Purple",
@@ -396,12 +390,6 @@ const productsData = [
     id: "price_1OPqReI1zNR8sZYDvUwcYYHB",
     imageSrc: p60,
     title: "BDay Bouquet",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPqTlI1zNR8sZYDgkk4myPo",
-    imageSrc: p61,
-    title: "Hello Kitty",
     price: "149.99",
   },
   {
@@ -419,13 +407,7 @@ const productsData = [
   {
     id: "price_1OPqVlI1zNR8sZYD4Rq57jN6",
     imageSrc: p64,
-    title: "Red Roses Butterflies",
-    price: "149.99",
-  },
-  {
-    id: "price_1OPqXPI1zNR8sZYDWWJvPVnZ",
-    imageSrc: p65,
-    title: "Halloween",
+    title: "RedRoses&Butter",
     price: "149.99",
   },
   {
