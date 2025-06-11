aac89ee05a8ea9e86ff00eba9b72806012a44824
Aldo Cornejo <aldocornejo138@yahoo.com>
2025-06-11 11:25:40 -0700

    update

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -140,11 +140,11 @@ function Hero() {
             <div className="right-content">
               Powered By:
               <a
-                href="https://www.seodevleads.com/"
+                href="https://aldahir-cornejo-portfolio.vercel.app/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <h1>SEODevLeads.com</h1>
+                <h1>Aldahir Cornejo</h1>
               </a>
             </div>
           </div>
